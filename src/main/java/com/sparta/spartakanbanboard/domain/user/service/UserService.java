9d01261c6a8b8acf2b9187fc38d3ca4c1d0ddcf5
pk_hyun <pkh954533@gmail.com>
2024-07-11 16:19:03 +0900

    ✨ update - #29 토큰 재생성 API 구현 및 로그인 기능 토큰dto를 이용하여 response 수정

--- src/main/java/com/sparta/spartakanbanboard/domain/user/service/UserService.java
+++ src/main/java/com/sparta/spartakanbanboard/domain/user/service/UserService.java
@@ -2,6 +2,7 @@ package com.sparta.spartakanbanboard.domain.user.service;
 
 import com.sparta.spartakanbanboard.domain.user.dto.LoginRequestDto;
 import com.sparta.spartakanbanboard.domain.user.dto.SignupRequestDto;
+import com.sparta.spartakanbanboard.domain.user.dto.TokenResponseDto;
 import com.sparta.spartakanbanboard.domain.user.entity.User;
 import com.sparta.spartakanbanboard.domain.user.entity.UserRole;
 import com.sparta.spartakanbanboard.domain.user.entity.UserStatus;
@@ -9,6 +10,8 @@ import com.sparta.spartakanbanboard.domain.user.repository.UserRepository;
 import com.sparta.spartakanbanboard.global.BusinessLogicException;
 import com.sparta.spartakanbanboard.global.dto.CommonResponseDto;
 import com.sparta.spartakanbanboard.global.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -59,9 +62,7 @@ public class UserService {
 
     //로그인
     public CommonResponseDto login(LoginRequestDto requestDto, HttpServletResponse response) {
-        User user = userRepository.findByUserName(requestDto.getUserName())
-                .orElseThrow(() -> new BusinessLogicException("사용자를 찾을 수 없습니다."));
-
+        User user = findByUserName(requestDto.getUserName());
 
         if (!passwordEncoder.matches(requestDto.getPassword(), user.getPassword())) {
             throw new BusinessLogicException("잘못된 비밀번호입니다.");
@@ -70,7 +71,8 @@ public class UserService {
         String refreshToken = jwtUtil.createRefreshToken(user.getUserName());
 
         // DB에 토큰 저장 및 유저 상태 변경
-        user.updateRefreshToken(refreshToken);
+        String substringRefreshToken = refreshToken.substring(JwtUtil.BEARER_PREFIX.length());
+        user.updateRefreshToken(substringRefreshToken);
         user.login();
         userRepository.save(user);
 
@@ -78,9 +80,14 @@ public class UserService {
         jwtUtil.addJwtToHeader(JwtUtil.AUTHORIZATION_HEADER, accessToken, response);
         jwtUtil.addJwtToHeader(JwtUtil.REFRESH_HEADER, refreshToken, response);
 
+        TokenResponseDto responseDto = TokenResponseDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+
         return CommonResponseDto.builder()
                 .msg("로그인 되었습니다.")
-                .data(accessToken)
+                .data(responseDto)
                 .build();
     }
 
@@ -95,8 +102,51 @@ public class UserService {
                 .build();
     }
 
+    public CommonResponseDto refresh(HttpServletRequest request, HttpServletResponse response) {
+        String refreshToken = jwtUtil.getTokenFromHeader(JwtUtil.REFRESH_HEADER, request);
+
+        if (!jwtUtil.validateToken(refreshToken)) {
+            throw new BusinessLogicException("리프레시 토큰도 만료되었습니다. 다시 로그인 해주세요.");
+        }
+
+        Claims info = jwtUtil.getUserInfoFromToken(refreshToken);
+        String userName = info.getSubject();
+
+        User user = findByUserName(userName);
+
+        if (!refreshToken.equals(user.getRefreshToken())) {
+            throw new BusinessLogicException("해당 유저의 리프레시 토큰이 일치하지 않습니다.");
+        }
+
+        String newAccessToken = jwtUtil.createAccessToken(user.getUserName(),user.getUserRole());
+        String newRefreshToken = jwtUtil.createRefreshToken(user.getUserName());
+
+        jwtUtil.addJwtToHeader(JwtUtil.AUTHORIZATION_HEADER, newAccessToken, response);
+        jwtUtil.addJwtToHeader(JwtUtil.REFRESH_HEADER, newRefreshToken, response);
+
+        String substringRefreshToken = newRefreshToken.substring(JwtUtil.BEARER_PREFIX.length());
+        user.updateRefreshToken(substringRefreshToken);
+        userRepository.save(user);
+
+        TokenResponseDto responseDto = TokenResponseDto.builder()
+                .accessToken(newAccessToken)
+                .refreshToken(newRefreshToken)
+                .build();
+
+        return CommonResponseDto.builder()
+                .msg("토큰이 재발급되었습니다.")
+                .data(responseDto)
+                .build();
+
+    }
+
     public User findByUserId(Long userId) {
         return userRepository.findById(userId).orElseThrow(() ->
                 new BusinessLogicException("해당 유저를 찾을 수 없습니다"));
+    }
+
+    public User findByUserName(String userName) {
+        return userRepository.findByUserName(userName).orElseThrow(() ->
+                new BusinessLogicException("해당 유저를 찾을 수 없습니다."));
     }
 }
