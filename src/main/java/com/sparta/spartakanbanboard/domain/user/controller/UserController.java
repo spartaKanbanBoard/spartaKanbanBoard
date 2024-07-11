f01a423c459c9c37d845e2ec42b9bc4a0ff510cb
pk_hyun <pkh954533@gmail.com>
2024-07-11 14:52:07 +0900

    ✨ update - #15 로그인 기능 구현

--- src/main/java/com/sparta/spartakanbanboard/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/spartakanbanboard/domain/user/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.sparta.spartakanbanboard.domain.user.controller;
 
+import com.sparta.spartakanbanboard.domain.user.dto.LoginRequestDto;
 import com.sparta.spartakanbanboard.domain.user.dto.SignupRequestDto;
 import com.sparta.spartakanbanboard.domain.user.service.UserService;
 import com.sparta.spartakanbanboard.global.dto.CommonResponseDto;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,6 +23,12 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<CommonResponseDto> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
         CommonResponseDto responseDto = userService.signup(signupRequestDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<CommonResponseDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        CommonResponseDto responseDto = userService.login(loginRequestDto, response);
         return ResponseEntity.ok().body(responseDto);
     }
 }
