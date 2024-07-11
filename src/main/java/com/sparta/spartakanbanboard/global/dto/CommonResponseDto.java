7bffb33066a4c5a197a7b6ab5803a756ef9c93fb
ggumi030 <tndus50843@gmail.com>
2024-07-11 09:57:43 +0900

    ♻️ refactor - CommonResponseDto 로 이름 변경

--- src/main/java/com/sparta/spartakanbanboard/global/dto/CommonResponseDto.java
+++ src/main/java/com/sparta/spartakanbanboard/global/dto/CommonResponseDto.java
@@ -1,15 +1,14 @@
 package com.sparta.spartakanbanboard.global.dto;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 
 @Getter
-public class CommonResDto<T> {
+public class CommonResponseDto<T> {
     private int code;
     private T data;
 
     @Builder
-    public CommonResDto(int code, T data){
+    public CommonResponseDto(int code, T data){
         this.code = code;
         this.data = data;
     }
