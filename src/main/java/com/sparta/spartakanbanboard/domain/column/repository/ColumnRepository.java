3b7b8eaf875e0cc2e6ecb6251e5792fd65ada73a
pk_hyun <pkh954533@gmail.com>
2024-07-11 12:21:26 +0900

    ♻️ refactor - Column -> KanbanColumn 예약어로인해 entity 명 수정

--- src/main/java/com/sparta/spartakanbanboard/domain/column/repository/ColumnRepository.java
+++ src/main/java/com/sparta/spartakanbanboard/domain/column/repository/ColumnRepository.java
@@ -1,7 +1,7 @@
 package com.sparta.spartakanbanboard.domain.column.repository;
 
-import com.sparta.spartakanbanboard.domain.column.entity.Column;
+import com.sparta.spartakanbanboard.domain.column.entity.KanbanColumn;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ColumnRepository extends JpaRepository<Column, Long> {
+public interface ColumnRepository extends JpaRepository<KanbanColumn, Long> {
 }
