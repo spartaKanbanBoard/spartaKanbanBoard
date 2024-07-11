edda46a2bd3dfc3f7e2e79d6375562599e9e9694
pk_hyun <pkh954533@gmail.com>
2024-07-11 12:17:37 +0900

    ✨ update - #15 security에 필요한 detailsImpl 및 serviceImpl 구현

---
+++ src/main/java/com/sparta/spartakanbanboard/global/security/UserDetailsImpl.java
@@ -0,0 +1,65 @@
+package com.sparta.spartakanbanboard.global.security;
+
+import com.sparta.spartakanbanboard.domain.user.entity.User;
+import com.sparta.spartakanbanboard.domain.user.entity.UserRole;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUserName();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        UserRole role = user.getUserRole();
+        String roleString = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(roleString);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/sparta/spartakanbanboard/global/security/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package com.sparta.spartakanbanboard.global.security;
+
+import com.sparta.spartakanbanboard.domain.user.entity.User;
+import com.sparta.spartakanbanboard.domain.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(userName)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + userName));
+
+        return new UserDetailsImpl(user);
+    }
+
+}
