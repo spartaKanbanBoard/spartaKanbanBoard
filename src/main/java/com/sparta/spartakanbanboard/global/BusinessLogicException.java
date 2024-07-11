d950cc3a061b9fc7249967141bebe4263e9cc057
ggumi030 <tndus50843@gmail.com>
2024-07-11 13:05:19 +0900

    🎉 add - #8 BusinessLogicException 클래스 추가

---
+++ src/main/java/com/sparta/spartakanbanboard/global/BusinessLogicException.java
@@ -0,0 +1,10 @@
+package com.sparta.spartakanbanboard.global;
+
+public class BusinessLogicException extends RuntimeException{
+
+    public BusinessLogicException(String message) {
+        super(message);
+    }
+
+
+}
