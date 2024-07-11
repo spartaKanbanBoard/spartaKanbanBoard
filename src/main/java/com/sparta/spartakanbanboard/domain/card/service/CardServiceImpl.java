386eed709fc5b19dbe50ef08ed7179ee5901dcd9
Hyungs0703 <hyungs0703@naver.com>
2024-07-11 15:26:56 +0900

    카드생성

--- src/main/java/com/sparta/spartakanbanboard/domain/card/service/CardServiceImpl.java
+++ src/main/java/com/sparta/spartakanbanboard/domain/card/service/CardServiceImpl.java
@@ -1,17 +1,1 @@
-package com.sparta.spartakanbanboard.domain.card.service;
-
-import com.sparta.spartakanbanboard.domain.card.dto.CreateCardRequestDto;
-import com.sparta.spartakanbanboard.domain.user.service.global.dto.CommonResponseDto;
-import com.sparta.spartakanbanboard.domain.user.service.global.security.UserDetailsImpl;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class CardServiceImpl {
-
-
-
-    public CommonResponseDto createCard(Long columnId, CreateCardRequestDto requestDto, UserDetailsImpl userDetails) {
-    }
-}
+package com.sparta.spartakanbanboard.domain.card.service;import com.sparta.spartakanbanboard.domain.card.dto.CreateCardRequestDto;import com.sparta.spartakanbanboard.domain.card.entity.Card;import com.sparta.spartakanbanboard.domain.card.repository.CardRepository;import com.sparta.spartakanbanboard.domain.column.dto.ColumnResponseDto;import com.sparta.spartakanbanboard.domain.column.repository.ColumnRepository;import com.sparta.spartakanbanboard.domain.user.repository.UserRepository;import com.sparta.spartakanbanboard.domain.user.service.UserService;import com.sparta.spartakanbanboard.domain.user.service.global.dto.CommonResponseDto;import com.sparta.spartakanbanboard.domain.user.service.global.security.UserDetailsImpl;import lombok.RequiredArgsConstructor;import org.springframework.stereotype.Service;@Service@RequiredArgsConstructorpublic class CardServiceImpl {    private final UserRepository userRepository;    private final CardRepository cardRepository;    private final ColumnRepository columnRepository;    private final UserService userService;    public CommonResponseDto createCard(Long columnId, CreateCardRequestDto requestDto, UserDetailsImpl userDetails) {        userService.findByUserId(userDetails.getUser().getId());        columnRepository.findById(columnId);        Card card = Card.of(requestDto);//        column.addCard(card);        cardRepository.save(card);        return CommonResponseDto.builder()            .msg("카드 생성이 완료되었습니다.")            .data(card)            .build();    }}
