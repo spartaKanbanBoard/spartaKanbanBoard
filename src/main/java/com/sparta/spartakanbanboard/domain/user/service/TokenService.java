8edcb63b438f8f8dc01790c0e36735f66087da0e
pk_hyun <pkh954533@gmail.com>
2024-07-11 18:11:15 +0900

    🎉 add - #33 TokenService 구현

---
+++ src/main/java/com/sparta/spartakanbanboard/domain/user/service/TokenService.java
@@ -0,0 +1,27 @@
+package com.sparta.spartakanbanboard.domain.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void saveRefreshToken(String username, String refreshToken) {
+        redisTemplate.opsForValue().set(username, refreshToken, 14, TimeUnit.DAYS);
+    }
+
+    public String getRefreshToken(String username) {
+        return redisTemplate.opsForValue().get(username);
+    }
+
+    public void deleteRefreshToken(String username) {
+        redisTemplate.delete(username);
+    }
+}
+
