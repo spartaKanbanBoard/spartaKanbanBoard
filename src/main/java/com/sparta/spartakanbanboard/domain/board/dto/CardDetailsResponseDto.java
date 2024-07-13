c3b34e4eb923ca76ef0ef5eee48e96a4a594bd87
ggumi030 <tndus50843@gmail.com>
2024-07-13 16:28:41 +0900

    ♻️ refactor - #48 endTime 필드 추가

--- src/main/java/com/sparta/spartakanbanboard/domain/board/dto/CardDetailsResponseDto.java
+++ src/main/java/com/sparta/spartakanbanboard/domain/board/dto/CardDetailsResponseDto.java
@@ -2,16 +2,18 @@ package com.sparta.spartakanbanboard.domain.board.dto;
 
 import com.sparta.spartakanbanboard.domain.card.entity.Card;
 import com.sparta.spartakanbanboard.domain.card.entity.State;
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
 public class CardDetailsResponseDto {
+    private String username;
     private String title;
     private String content;
-    private String username;
     private State state;
+    private LocalDateTime endTime;
     private int sequence;
 
     public static CardDetailsResponseDto of(Card card) {
@@ -21,6 +23,7 @@ public class CardDetailsResponseDto {
             .username(card.getUsername())
             .state(card.getState())
             .sequence(card.getSequence())
+            .endTime(card.getEndTime())
             .build();
     }
 }
